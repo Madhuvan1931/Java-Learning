bd1bba687a1484e350cecf49767e5451ad42a1a9
Madhuvan Singh <118827902+Madhuvan1931@users.noreply.github.com>
2025-08-15 13:47:04 +0530

    Delete do_while_loop.java

--- do_while_loop.java
+++
@@ -1,9 +0,0 @@
-public class do_while_loop {
-    public static void main(String[] args) {
-        int counter = 1;
-        do{
-            System.out.println(counter + " Hello Madhuvan Singh");
-            counter++;
-        }while(counter <=10);
-    }
-}
